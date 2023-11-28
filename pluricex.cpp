2155e546a2b9976597ac40e340b9becb61f77d00
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-11-28 17:16:27 +0200

    Create pluricex.cpp
    transfer pc -> laptop

---
+++ pluricex.cpp
@@ -0,0 +1,101 @@
+#include <fstream>
+#include <cstring>
+#define NR_MAXIM_ELEVI 22
+#define NR_MAXIM_ELEVI_IN_ECHIPA 8
+#define NR_MAXIM_DISCIPLINE 10
+using namespace std;
+ofstream scrie("pluricex.out");
+int nrElevi,
+    nrEleviInEchipa,
+    nrDiscipline,
+    echipa[NR_MAXIM_ELEVI_IN_ECHIPA];
+bool elev[NR_MAXIM_ELEVI][NR_MAXIM_DISCIPLINE];
+void citesteDateleDeIntrare();
+void determinaToateEchipele(int, int);
+int main()
+{
+    citesteDateleDeIntrare();
+    determinaToateEchipele(0, 0);
+    scrie.close();
+    return 0;
+}
+void afiseazaEchipa()
+{
+    for (int i = 0; i < nrEleviInEchipa; i++)
+    {
+        scrie << echipa[i] << ' ';
+    }
+    scrie << endl;
+}
+bool indeplinesteConditiaProblemei()
+{
+    // verifica daca pentru fiecare disciplina
+    // exista in echipa cel putin un membru
+    // care sa o studieze
+    bool uz[nrDiscipline];
+    memset(uz, 0, sizeof(uz));
+    int i, j;
+    for (i = 0; i < nrEleviInEchipa; i++)
+    {
+        for (j = 0; j < nrDiscipline; j++)
+        {
+            if (elev[i][j])
+            {
+                uz[j] = 1;
+            }
+        }
+    }
+    for (i = 0; i < nrDiscipline; i++)
+    {
+        if (uz[i] == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+void determinaToateEchipele(
+    int nrOrdineElev,
+    int nrEleviAlesiPanaAcum)
+{
+    scrie << nrOrdineElev << ' ' << nrEleviAlesiPanaAcum << endl;
+    echipa[nrEleviAlesiPanaAcum] = nrOrdineElev;
+    if (nrEleviAlesiPanaAcum == nrEleviInEchipa)
+    {
+        scrie << "verifica sol" << endl;
+        afiseazaEchipa();
+        /*if (indeplinesteConditiaProblemei())
+        {
+            afiseazaEchipa();
+        }*/
+    }
+    else
+    {
+        scrie << "cauta sol" << endl;
+        for (int i = nrOrdineElev+1; i < nrElevi; i++)
+        {
+            determinaToateEchipele(
+                i,
+                nrEleviAlesiPanaAcum+1);
+        }
+    }
+}
+void citesteDateleDeIntrare()
+{
+    ifstream citeste("pluricex.in");
+    citeste >> nrElevi
+            >> nrEleviInEchipa
+            >> nrDiscipline;
+    for (int i = 0; i < nrElevi; i++)
+    {
+        int nrDisciplineStudiate;
+        citeste >> nrDisciplineStudiate;
+        for (int j = 0; j < nrDisciplineStudiate; j++)
+        {
+            int disciplina;
+            citeste >> disciplina;
+            elev[i][disciplina] = 1;
+        }
+    }
+    citeste.close();
+}
